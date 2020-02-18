c0fc4d283bedc295699cf5d3b453bcbe85cf7158
Alexander Shiryaev <ash@dvhb.ru>
2020-02-18 11:06:10 +0300

    fix: hide splash screen in storybook

--- template/storybook/index.js
+++ template/storybook/index.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
-import { loadStories } from './storyLoader';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import './rn-addons';
+import SplashScreen from 'react-native-splash-screen';
 
+import { loadStories } from './storyLoader';
+import './rn-addons';
 import { stylesheetConfig } from '../src/boot/stylesheetConfig';
 
 // import stories
@@ -11,7 +13,17 @@ configure(() => loadStories(), module);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUI = getStorybookUI({});
+
+export class StorybookUIRoot extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return <StorybookUI />;
+  }
+}
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
