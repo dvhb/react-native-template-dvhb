e5afeed0a78bbd87cf912340e3995473462ba3ed
Aleksey Subbotenko <asu@dvhb.ru>
2020-08-04 16:58:13 +0300

    feat: update openapi-generator

--- template/src/api/models/index.ts
+++ template/src/api/models/index.ts
@@ -4,3 +4,12 @@ export * from './IOrder';
 export * from './IPet';
 export * from './ITag';
 export * from './IUser';
+
+export enum FetchState {
+  Initial = 0,
+  InProgress = 1,
+  Finished = 2,
+  Error = 3,
+  NotFound = 4,
+  Obsolete = 5,
+}
